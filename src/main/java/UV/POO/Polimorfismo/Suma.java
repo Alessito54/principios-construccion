8519485c8e88a163c8053867b05c82d6b8a3bb61
Alessito54 <arielpedraza10@outlook.com>
2026-02-18 12:37:22 -0600

    se agrego clase con sus metodos polimorficos

---
+++ src/main/java/UV/POO/Polimorfismo/Suma.java
@@ -0,0 +1,13 @@
+package UV.POO.Polimorfismo;
+
+public class Suma {
+    public void sumar(int a, int b) {
+        System.out.println("La suma de " + a + " y " + b + " es: " + (a + b));
+    }
+    public double sumar(double a, double b, double c) {
+       return a + b + c;
+    }
+    public int sumar(String mensaje,byte a, byte b) {
+        return a + b;
+    }
+}
